ca6b41853dc0db72fd9bf7370598b717cb5a4996
Pablo Roman <emanuelpabloroman@gmail.com>
2020-09-16 20:31:04 -0300

    Se agrego Autenticacion con google

--- public/js/auth/authController.js
+++ public/js/auth/authController.js
@@ -1,12 +1,11 @@
 $(() => {    
-
+    const objAuth = new Autenticacion()
     //$("#authFB").click(() => );
 
     $("#btnRegistroEmail").click(() => {
         const nombres = $('#nombreContactoReg').val();
         const email = $('#emailContactoReg').val();
         const password = $('#passwordReg').val();
-        // TODO : LLamar crear cuenta con email
         const auth = new Autenticacion();
         auth.crearCuentaEmailPass(email, password, nombres)
     });
@@ -14,12 +13,11 @@ $(() => {
     $("#btnInicioEmail").click(() => {
         const email = $('#emailSesion').val();
         const password = $('#passwordSesion').val();
-        // TODO : LLamar auth cuenta con email
         const auth = new Autenticacion()
         auth.autEmailPass(email, password)
     });
 
-    //$("#authGoogle").click(() => //AUTH con GOOGLE);
+    $("#authGoogle").click(() => objAuth.authCuentaGoogle());
 
     //$("#authTwitter").click(() => //AUTH con Twitter);
 
