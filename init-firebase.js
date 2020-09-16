9d6acb18b8d5a6edcc8e5270e95868e4585927e5
Pablo Roman <emanuelpabloroman@gmail.com>
2020-09-16 15:41:11 -0300

    blog con firebase

---
+++ init-firebase.js
@@ -0,0 +1,13 @@
+// TODO: Replace the following with your app's Firebase project configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyA377NNbpkCf8CjnoO2A9TOOO69cEXzXP0",
+    authDomain: "blogpabloroman.firebaseapp.com",
+    databaseURL: "https://blogpabloroman.firebaseio.com",
+    projectId: "blogpabloroman",
+    storageBucket: "blogpabloroman.appspot.com",
+    messagingSenderId: "614326028268",
+    appId: "1:614326028268:web:6f62498a979844355ae2c9",
+    measurementId: "G-5KWJCVZRVX"  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ public/js/auth/autenticacion.js
@@ -0,0 +1,65 @@
+class Autenticacion {
+  autEmailPass (email, password) {
+    firebase.auth().signInWithEmailAndPassword(email,password)
+    .then(result => {
+      if (result.user.sendEmailVerification) {
+        $('#avatar').attr('src', 'imagenes/usuario_auth.png')
+        Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)      
+      } else {
+        firebase.auth().signOut()
+        Materialize.toast(`Por favor realiza la verificacion de la cuenta`, 5000)
+      }
+    })
+
+    $('.modal').modal('close')
+  }
+
+  crearCuentaEmailPass (email, password, nombres) {
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(result => {
+        result.user.updateProfile({
+          displayName : nombres
+        })
+        //verifica el mail
+        const configuracion ={
+          url :'http://localhost:3000/'
+        }
+        result.user.sendEmailVerification(configuracion)
+        .catch(error =>{
+          console.error(error)
+          Materialize.toast(error.message, 4000)
+        })
+        firebase.auth().signOut()
+
+        Materialize.toast(
+          `Bienvenido ${nombres}, debes realizar el proceso de verificación`,
+          4000
+        )
+        $('.modal').modal('close')
+
+      })
+      .catch(error =>{
+        console.error(error)
+        Materialize.toast(error.message, 4000)
+      })
+
+  }
+
+  authCuentaGoogle () {
+    //$('#avatar').attr('src', result.user.photoURL)
+    //$('.modal').modal('close')
+    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authCuentaFacebook () {
+    //$('#avatar').attr('src', result.user.photoURL)
+    //$('.modal').modal('close')
+    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+  }
+
+  authTwitter () {
+    // TODO: Crear auth con twitter
+  }
+}
